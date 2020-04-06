45ca776ecbe658ec32760986b0585167a821577d
Alexandra Trofimova <alazingara@inbox.ru>
2020-04-06 11:30:00 +0300

    5. Реализовать класс Tracker[#198803]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,29 +1,91 @@
 package ru.job4j.tracker;
 
-public class Tracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Arrays;
 
+public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private final Item[] items = new Item[100];
+
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    /**
+     * Метод, реализующий добавление заявки в хранилище
+     * @param item новая заявка
+     */
     public Item add(Item item) {
-        item.setId(generateId());
-        items[size++] = item;
+        if (item.getName() != null) {
+            item.setId(generateId());
+            this.items[position++] = item;
+        }
         return item;
     }
 
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     * @return Уникальный ключ.
+     */
     private String generateId() {
-        return String.valueOf(ids++);
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+    /**
+     * Метод, возвращающий копию массива без пустых ячеек
+     */
+    public Item[] findAll() {
+        return Arrays.copyOf(items, position);
+    }
+    /**
+     * Метод, выполняющий поиск заявки по имени
+     * @param key имя заявки, которую ищет метод
+     */
+    public Item[] findByName(String key) {
+        Item[] equalNames = new Item[position];
+        int size = 0;
+        for (int index = 0; index < position; index++) {
+            Item tmp = this.items[index];
+            if (tmp.getName().equals(key)) {
+                equalNames[size] = tmp;
+                size++;
+            }
+        }
+        return Arrays.copyOf(equalNames, size);
+    }
+    /**
+     * Метод, выполняющий поиск заявки по уникальному ID
+     * @param id - идентификатор заявки, которую ищет метод
+     */
+    public Item findById(String id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
-    public Item findById(String id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId().equals(id)) {
-                rsl = item;
+    private int indexOf(String id) {
+        int rsl = -1;
+        for (int index = 0; index < position; index++) {
+            if (items[index].getId().equals(id)) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public boolean replace(String id, Item item) {
+        int index = indexOf(id);
+        boolean res = false;
+        if (index != -1) {
+            item.setId(id);
+            this.items[index] = item;
+            res = true;
+        }
+        return res;
     }
 }
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -13,4 +13,58 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void whenFindAllThenReturnAllWithoutNull() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item(null);
+        Item item4 = new Item("test4");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item test[] = tracker.findAll();
+        Item result[] = {item1, item2, item4};
+        assertThat(result, is(test));
+    }
+
+    @Test
+    public void whenFindByNameThanTwoEqualNames() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        Item item3 = new Item("test3");
+        Item item4 = new Item("test1");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        tracker.add(item4);
+        Item test[] = tracker.findByName("test1");
+        Item result[] = {item1, item4};
+        assertThat(result, is(test));
+    }
+
+    @Test
+    public void whenFindById() {
+        Tracker tracker = new Tracker();
+        Item item1 = new Item("test1");
+        Item item2 = new Item("test2");
+        tracker.add(item1);
+        tracker.add(item2);
+        Item test = tracker.findById(item1.getId());
+        assertThat(test.getId(), is(item1.getId()));
+    }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        tracker.add(bug);
+        String id = bug.getId();
+        Item bugWithDesc = new Item("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
 }
